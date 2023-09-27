9dd06a7eed0f75b99eb67b065d1b3eedc0d28e2d
richarjami <140350007+richarjami@users.noreply.github.com>
2023-09-28 10:13:30 +1300

    28/09

--- music_starter.js
+++ music_starter.js
@@ -6,11 +6,39 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   rectMode(CENTER)
   textSize(24);
 
+  console.log(counter);
+
    let bar_spacing = height / 10;
    let bar_height = width / 12;
    let bar_pos_x = width / 2;
  
    let vocalHeight = map(vocal, 0,100, 0,50)
+
+   //Gradient BG
+   let blueColorNew = color(0, 0, 0);
+   let blueColorOld = color(68, 126, 227);
+   let skyColorNew = color(37, 12, 1);
+   let skyColorOld = color(241, 130, 31);
+   strokeWeight(3);
+
+   for (let bg = 0; bg < 1080; bg++) {
+    
+    let gradientAmount = map(bg,0,1080,0,1)
+    let orangeColorGradient = map(counter,0,1225,0,1)
+    let orangeColor = lerpColor(skyColorNew, skyColorOld, orangeColorGradient)
+    let blueColorGradient = map(counter,0,1225,0,1)
+    let blueColor = lerpColor(blueColorNew, blueColorOld, blueColorGradient)
+    let strokeColor = lerpColor(blueColor, orangeColor, gradientAmount)
+    stroke(strokeColor)
+
+    line(0,bg, width,bg)
+   }
+
+   strokeWeight(0)
+
+   if (counter > 1225) {
+      rect(0,0,100,100)
+   }
 
    // vocal bar is red
    fill(200, 0, 0);
