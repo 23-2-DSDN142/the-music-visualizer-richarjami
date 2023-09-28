a773ca1d861ca57553762fb3b5b061661435c0e0
richarjami <140350007+richarjami@users.noreply.github.com>
2023-09-28 16:47:44 +1300

    28/09

--- music_starter.js
+++ music_starter.js
@@ -1,22 +1,38 @@
+let firstRun = true
+let cityFront;
+let cityMid;
+let cityBack;
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   background(20)
-  textFont('Helvetica'); // please use CSS safe fonts
+  textFont('Verdana'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
 
-  console.log(counter);
 
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
-   let vocalHeight = map(vocal, 0,100, 0,50)
 
-   //Gradient BG
-   let blueColorNew = color(0, 0, 0);
-   let blueColorOld = color(68, 126, 227);
+
+
+
+//Image Loading
+  if (firstRun) {
+   cityFront = loadImage('cityfront.png');
+   cityMid = loadImage('citymid.png');
+   cityBack = loadImage('cityback.png');
+
+   firstRun = false
+  }
+
+
+
+
+
+
+//Gradient BG
+
+   let blueColorNew = color(31, 15, 15);
+   let blueColorOld = color(220, 106, 103);
    let skyColorNew = color(37, 12, 1);
    let skyColorOld = color(241, 130, 31);
    strokeWeight(3);
@@ -34,39 +50,94 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     line(0,bg, width,bg)
    }
 
-   strokeWeight(0)
 
-   if (counter > 1225) {
-      rect(0,0,100,100)
+
+
+
+
+
+//City
+
+   strokeWeight(1)
+   //let imageHeights = sin(1*counter)*100
+   let imageHeights = counter*0.15
+   
+
+   image(cityBack,0,imageHeights*0.25);
+
+   let cityFogBottomBlack = color(0,0,0,255);
+   let cityFogBottomOrange = color(241,130,31,255);
+   let cityFogTop = color(241,130,31,0);
+
+   for (let fog = 0; fog < 1080; fog++) {
+
+   let fogGradientAmount = map(fog,0,1080,0,1);
+   let fogCol = map(counter, 0,1225, 0,1);
+   let cityFogBottom = lerpColor(cityFogBottomBlack, cityFogBottomOrange, fogCol);
+   let cityFogColor = lerpColor(cityFogTop, cityFogBottom, fogGradientAmount);
+
+   stroke(cityFogColor)
+
+   line(0,fog, width,fog)
+   }
+  
+  image(cityMid,0,imageHeights*0.5);
+
+  for (let fog = 0; fog < 1080; fog++) {
+
+   let fogGradientAmount = map(fog,0,1080,0,1);
+   let fogCol = map(counter, 0,1225, 0,1);
+   let cityFogBottom = lerpColor(cityFogBottomBlack, cityFogBottomOrange, fogCol);
+   let cityFogColor = lerpColor(cityFogTop, cityFogBottom, fogGradientAmount);
+
+   stroke(cityFogColor)
+
+   line(0,fog, width,fog)
    }
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, vocalHeight);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
+  image(cityFront,0,imageHeights);
+
+
+
+
+
+
+//Lyrics
    textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+
+   let vocalsCol = map(vocal, 0,100, 100,255);
+   fill(vocalsCol, vocalsCol, vocalsCol, vocalsCol);
+   strokeWeight(0);
+   stroke(5);
+   textSize(60);
+   text(words, width/2, 1020);
+
+
+
+
+
+
+
+//Now Playing
+
+   let nowPlayingVocal = map(vocal, 0, 100, 0, -20);
+   let nowPlayingDrums = map(drum, 0, 100, 0, -20);
+   let nowPlayingBass = map(bass, 0, 100, 0, -20);
+   let nowPlayingOther = map(other, 0, 100, 0, -20);
+
+   if (counter > 1225) {
+   fill(242, 156, 107);
+   textAlign(LEFT);
+   textSize(30);
+   text("Two Door Cinema Club - Sun", 20,50);
+
+   rectMode(CORNERS);
+
+   rect(480, 50, 490, 50+nowPlayingVocal)
+   rect(495, 50, 505, 50+nowPlayingDrums)
+   rect(510, 50, 520, 50+nowPlayingBass)
+   rect(525, 50, 535, 50+nowPlayingOther)
+
+
+   }
 }
